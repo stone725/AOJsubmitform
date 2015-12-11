94c669632a6dbe0b66247c444bc0ebf9ada8d2a6
stone725 <stone_725@outlook.jp>
2015-12-12 08:08:39 +0900

    TwitterのコードをHTMLソースから取得して自動認証することがうまい具合にできなくなっていたので修正
    認証コードが囲まれた部分が<CODE>ではなく<code>になっていたのでどちらでも対応することができるようにしました

--- AOJsubmitform/TwitterAttestationForm.cs
+++ AOJsubmitform/TwitterAttestationForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace AOJsubmitform
 {
@@ -31,13 +32,21 @@ namespace AOJsubmitform
 
     private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
     {
-      string code = webBrowser1.DocumentText;//codeにソースコードを入れる
+      string code = webBrowser1.DocumentText;//codeにソースコードを入れ
       if (-1 != code.IndexOf("<CODE>", StringComparison.Ordinal))//ソース内に「<code>」が存在するか
       {
         webBrowser1.Visible = false;//コードを取得したので,認証画面を消す
         int loc = code.IndexOf("<CODE>", StringComparison.Ordinal);//「<code>」の位置を取得.
         MainForm.TwitterVerifier = code.Substring(loc, 13);//「<code>」の位置から13文字分を取り出す.
         MainForm.TwitterVerifier = MainForm.TwitterVerifier.Replace("<CODE>", "");//「<code>」を削除
+        GetToken();//トーキンを取得.
+      }
+      else if (-1 != code.IndexOf("<code>", StringComparison.Ordinal))//ソース内に「<code>」が存在するか
+      {
+        webBrowser1.Visible = false;//コードを取得したので,認証画面を消す
+        int loc = code.IndexOf("<code>", StringComparison.Ordinal);//「<code>」の位置を取得.
+        MainForm.TwitterVerifier = code.Substring(loc, 13);//「<code>」の位置から13文字分を取り出す.
+        MainForm.TwitterVerifier = MainForm.TwitterVerifier.Replace("<code>", "");//「<code>」を削除
         GetToken();//トーキンを取得.
       }
     }
